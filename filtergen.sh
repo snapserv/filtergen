70a632418db4e77b65e2dd55963ecac103467d90
Pascal Mathis <mail@pascalmathis.com>
2021-02-22 12:51:43 +0100

    feat: preserve generated file on early exit for manual inspection

--- filtergen.sh
+++ filtergen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -euo pipefail
+set -eu
 
 # Determine path to script
 SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
@@ -18,9 +18,13 @@ MAX_DELTA_PERCENTAGE=20
 # Load user configuration if available
 if [ -r "${SCRIPT_PATH}/filtergen.conf" ]; then
 	set -o allexport
+	# shellcheck source=/dev/null
 	. "${SCRIPT_PATH}/filtergen.conf"
 	set +o allexport
 fi
+
+# Additional default configuration (based on previous values)
+NEW_PREFIXSETS_FILE="${NEW_PREFIXSETS_FILE:-${PREFIXSETS_FILE}.new}"
 
 # Check if force flag has been passed
 ignore_delta="$([ "${1:-}" = "force" ] && echo "yes" || echo "no")"
@@ -49,7 +53,7 @@ fi
 
 # Prepare temporary file for generation
 genfile="$(mktemp /tmp/filtergen.XXXXXX)"
-trap "rm -f '${genfile}'" EXIT
+trap 'rm -f "${genfile}"' EXIT
 
 # Fetch all configured prefix sets
 prefixsets="$(sed -En 's/^.*[[:space:]]+prefix-set (irr(4|6)-as([0-9]+))[[:space:]]+#[[:space:]]+([^ \t]+)[[:space:]]*$/\1 ipv\2 \3 \4/p' "${OPENBGPD_CONFIG}")"
@@ -96,6 +100,9 @@ echo "${prefixsets}" | while IFS= read -r line; do
 	fi
 done
 
+# Create copy of generated file for manual inspection
+cp -f "${genfile}" "${NEW_PREFIXSETS_FILE}"
+
 # Diff against current version and calculate delta
 current_lines="$(wc -l "${PREFIXSETS_FILE}" | awk '{$1=$1;print $1}' || exit 0)"
 changed_lines="$(sdiff -W -b -s "${PREFIXSETS_FILE}" "${genfile}" | wc -l | awk '{$1=$1;print $1}' || exit 0)"
@@ -109,6 +116,7 @@ echo "> Statistics: Changed ${changed_lines} of ${current_lines} lines with ${de
 
 # Abort if delta is zero
 if [ "${delta_percentage}" -eq "0" ] && [ "${changed_lines}" -eq "0" ]; then
+	echo "> The generated file has been placed at [${NEW_PREFIXSETS_FILE}] for manual inspection."
 	echo "> Everything already up-to-date, exiting now..."
 	exit 0
 fi
@@ -116,13 +124,14 @@ fi
 # Abort if delta is above threshold and not being ignored
 if [ "${ignore_delta}" != "yes" ] && [ "${delta_percentage}" -gt "${MAX_DELTA_PERCENTAGE}" ]; then
 	echo "> Too many changes (over ${MAX_DELTA_PERCENTAGE}% delta), aborting generation..."
+	echo "> The generated file has been placed at [${NEW_PREFIXSETS_FILE}] for manual inspection."
 	echo "> Run as [${0} force] to force generation. Exiting now!"
 	exit 3
 fi
 
 # Generate fake config for standalone verification
 tmpcfg="$(mktemp /tmp/filtergen.XXXXXX)"
-trap "rm -f '${tmpcfg}'" 0 2 3 15
+trap 'rm -f "${tmpcfg}"' 0 2 3 15
 echo "AS 1" > "${tmpcfg}"
 cat "${genfile}" >> "${tmpcfg}"
 
@@ -130,6 +139,7 @@ echo "> Verifying built configuration..."
 if ! bgpd -nf "${tmpcfg}"; then
 	echo "> Could not verify configuration, please check the output..."
 	cat "${tmpcfg}"
+	echo "> The generated file has also been placed at [${NEW_PREFIXSETS_FILE}] for manual inspection."
 	echo "> Exiting now!"
 	exit 4
 fi
@@ -142,9 +152,15 @@ mv "${genfile}" "${PREFIXSETS_FILE}"
 echo "> Verifying final configuration..."
 if ! bgpd -n; then
 	echo "> Could not verify final configuration, please check the output..."
+	echo "> The generated file has also been placed at [${NEW_PREFIXSETS_FILE}] for manual inspection."
 	echo "> Restoring old config and exiting now!"
 	mv "${PREFIXSETS_FILE}.bak" "${PREFIXSETS_FILE}"
 	exit 4
+fi
+
+# Remove previous copy of generated configuration
+if [ -f "${NEW_PREFIXSETS_FILE}" ]; then
+	rm -f "${NEW_PREFIXSETS_FILE}"
 fi
 
 # Reload daemon configuration
